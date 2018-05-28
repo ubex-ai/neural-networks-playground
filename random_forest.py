0c7081eae91e1bb5c33e1b85ac49e763bdc789f6
Dan Gartman <dan.gartman@hotmail.com>
2018-05-28 20:54:45 +0100

    train and eval

--- random_forest.py
+++ random_forest.py
@@ -32,3 +32,44 @@ def build_estimator(model_dir):
   return random_forest.TensorForestEstimator(
       params, graph_builder_class=graph_builder_class, model_dir=model_dir)
 
+
+def train_and_eval():
+  """Train and evaluate the model."""
+  model_dir = tempfile.mkdtemp() if not FLAGS.model_dir else FLAGS.model_dir
+  print('model directory = %s' % model_dir)
+
+  est = build_estimator(model_dir)
+
+  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=False)
+
+  train_input_fn = numpy_io.numpy_input_fn(
+      x={'images': mnist.train.images},
+      y=mnist.train.labels.astype(numpy.int32),
+      batch_size=FLAGS.batch_size,
+      num_epochs=None,
+      shuffle=True)
+  est.fit(input_fn=train_input_fn, steps=None)
+
+  metric_name = 'accuracy'
+  metric = {
+      metric_name:
+          metric_spec.MetricSpec(
+              eval_metrics.get_metric(metric_name),
+              prediction_key=eval_metrics.get_prediction_key(metric_name))
+  }
+
+  test_input_fn = numpy_io.numpy_input_fn(
+      x={'images': mnist.test.images},
+      y=mnist.test.labels.astype(numpy.int32),
+      num_epochs=1,
+      batch_size=FLAGS.batch_size,
+      shuffle=False)
+
+  results = est.evaluate(input_fn=test_input_fn, metrics=metric)
+  for key in sorted(results):
+    print('%s: %s' % (key, results[key]))
+
+
+def main(_):
+  train_and_eval()
+
