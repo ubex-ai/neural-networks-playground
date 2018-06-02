b8dd623a2279937f53cf5f4f0f00ee71af0b282f
Dan Gartman <dan.gartman@hotmail.com>
2018-06-02 21:03:40 +0100

    download iris

--- iris_classifier.py
+++ iris_classifier.py
@@ -17,3 +17,16 @@ IRIS_TEST_URL = 'http://download.tensorflow.org/data/iris_test.csv'
 
 FEATURE_KEYS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 
+
+def maybe_download_iris_data(file_name, download_url):
+  """Downloads the file and returns the number of data."""
+  if not os.path.exists(file_name):
+    urlretrieve(download_url, file_name)
+
+  # The first line is a comma-separated string. The first one is the number of
+  # total data in the file.
+  with open(file_name, 'r') as f:
+    first_line = f.readline()
+  num_elements = first_line.split(',')[0]
+  return int(num_elements)
+
