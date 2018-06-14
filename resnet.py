b3c82305fea21a082127f09a5c11debb3a053da9
Dan Gartman <dan.gartman@hotmail.com>
2018-06-14 20:38:55 +0100

    bottleneck groups

--- resnet.py
+++ resnet.py
@@ -54,3 +54,43 @@ def res_net_model(features, labels, mode):
         filters=groups[0].num_filters,
         kernel_size=1,
         padding='valid')
+
+  # Create the bottleneck groups, each of which contains `num_blocks`
+  # bottleneck groups.
+  for group_i, group in enumerate(groups):
+    for block_i in range(group.num_blocks):
+      name = 'group_%d/block_%d' % (group_i, block_i)
+
+      # 1x1 convolution responsible for reducing dimension
+      with tf.variable_scope(name + '/conv_in'):
+        conv = tf.layers.conv2d(
+            net,
+            filters=group.num_filters,
+            kernel_size=1,
+            padding='valid',
+            activation=tf.nn.relu)
+        conv = tf.layers.batch_normalization(conv, training=training)
+
+      with tf.variable_scope(name + '/conv_bottleneck'):
+        conv = tf.layers.conv2d(
+            conv,
+            filters=group.bottleneck_size,
+            kernel_size=3,
+            padding='same',
+            activation=tf.nn.relu)
+        conv = tf.layers.batch_normalization(conv, training=training)
+
+      # 1x1 convolution responsible for restoring dimension
+      with tf.variable_scope(name + '/conv_out'):
+        input_dim = net.get_shape()[-1].value
+        conv = tf.layers.conv2d(
+            conv,
+            filters=input_dim,
+            kernel_size=1,
+            padding='valid',
+            activation=tf.nn.relu)
+        conv = tf.layers.batch_normalization(conv, training=training)
+
+      # shortcut connections that turn the network into its counterpart
+      # residual function (identity shortcut)
+      net = conv + net
