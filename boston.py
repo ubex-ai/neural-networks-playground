fcd12113eff2640a3ddbda48ec292480e18907cb
Dan Gartman <dan.gartman@hotmail.com>
2018-06-27 21:11:48 +0100

    split and scale

--- boston.py
+++ boston.py
@@ -16,6 +16,13 @@ def main(unused_argv):
   boston = datasets.load_boston()
   x, y = boston.data, boston.target
 
+  # Split dataset into train / test
+  x_train, x_test, y_train, y_test = model_selection.train_test_split(
+      x, y, test_size=0.2, random_state=42)
+
+  # Scale data (training set) to 0 mean and unit standard deviation.
+  scaler = preprocessing.StandardScaler()
+  x_train = scaler.fit_transform(x_train)
 
 if __name__ == '__main__':
   tf.app.run()
