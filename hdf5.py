ffb54c8ed6dcf61c7c1c676474731802213b0823
Dan Gartman <dan.gartman@hotmail.com>
2018-07-05 22:28:24 +0100

    hdf5 network

--- hdf5.py
+++ hdf5.py
@@ -19,5 +19,14 @@ def main(unused_argv):
   x_train, x_test, y_train, y_test = model_selection.train_test_split(
       iris.data, iris.target, test_size=0.2, random_state=42)
 
+  # Note that we are saving and load iris data as h5 format as a simple
+  # demonstration here.
+  h5f = h5py.File('/tmp/test_hdf5.h5', 'w')
+  h5f.create_dataset('X_train', data=x_train)
+  h5f.create_dataset('X_test', data=x_test)
+  h5f.create_dataset('y_train', data=y_train)
+  h5f.create_dataset('y_test', data=y_test)
+  h5f.close()
+
 if __name__ == '__main__':
   tf.app.run()
